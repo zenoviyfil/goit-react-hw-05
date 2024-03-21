e2c860a41ba15fc03aa4bc282fffb5c87d212274
Zenoviy Fil <98259328+zenoviyfil@users.noreply.github.com>
2024-03-21 11:13:19 +0100

    api service

---
+++ src/api-service.js
@@ -0,0 +1,52 @@
+import axios from 'axios'
+
+axios.defaults.baseURL = "https://api.themoviedb.org/";
+
+const options = {
+  headers: {
+    accept: 'application/json',
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZTcyNmMyN2VlZTc4NDNiMmNiMGI0MTM4ZjYyMzdhYSIsInN1YiI6IjY1ZmI2NmJiNWJlMDBlMDE3YzZlYmY5NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.42_XviBHjDMxsDUsH-pdyIs42EoweLsXF82sUA51EoA",
+  },
+};
+
+const trendReq = async () => {
+    const res = await axios.get('trending/movie/week', options)
+
+    return res.data.results
+}
+
+const queryReq = async (query) => {
+    const res = await axios.get(
+      `search/movie?include_adult=true&page=1&query=${query}`,
+      options
+    );
+
+    return res.data.results
+}
+
+const movieDetailsReq = async (movieId) => {
+    const res = await axios.get(`movie/${movieId}`, options)
+
+    return res.data.results
+}
+
+const movieCastReq = async (movieId) => {
+    const res = await axios.get(`movie/${movieId}/credits`, options)
+
+    return res.data.results
+}
+
+const movieReviewsReq = async (movieId) => {
+    const res = await axios.get(`movie/${movieId}/reviews`, options)
+
+    return res.data.results
+}
+
+export { 
+    trendReq,
+    queryReq,
+    movieDetailsReq,
+    movieCastReq,
+    movieReviewsReq
+ }
